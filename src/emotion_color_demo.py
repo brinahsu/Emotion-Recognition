0e4d9a87f53d7d1caf6fcc47317421142fe65a4a
brinahsu <62058997+brinahsu@users.noreply.github.com>
2021-03-01 21:15:47 +0800

    Add files via upload

--- src/emotion_color_demo.py
+++ src/emotion_color_demo.py
@@ -12,10 +12,10 @@ from utils.inference import draw_bounding_box
 from utils.inference import apply_offsets
 from utils.inference import load_detection_model
 from utils.preprocessor import preprocess_input
-
+from keras.preprocessing import image
 # parameters for loading data and images
-detection_model_path = '../trained_models/detection_models/haarcascade_frontalface_default.xml'
-emotion_model_path = '../trained_models/emotion_models/fer2013_my_smallerCNN_new_trained.hdf5'
+detection_model_path = '../trained_models/haarcascade_frontalface_default.xml'
+emotion_model_path = '../trained_models/emotion_models/mymodel.h5'
 emotion_labels = get_labels('fer2013')
 
 # hyper-parameters for bounding boxes shape
@@ -25,12 +25,13 @@ emotion_offsets = (20, 40)
 # loading models
 face_detection = load_detection_model(detection_model_path)
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(512, activation=tf.nn.relu),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation=tf.nn.relu),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
-emotion_classifier = tf.keras.models.load_model(emotion_model_path, compile=False)
+emotion_classifier = tf.keras.models.load_model(
+    emotion_model_path, compile=False)
 
 # getting input model shapes for inference
 emotion_target_size = emotion_classifier.input_shape[1:3]
@@ -46,21 +47,23 @@ while True:
     gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
     rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
     faces = detect_faces(face_detection, gray_image)
-    #print(faces)
+    # print(faces)
     for face_coordinates in faces:
         print(face_coordinates)
         x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
-        gray_face = gray_image[y1:y2, x1:x2]
+        gray_face = rgb_image[y1:y2, x1:x2]
         try:
             gray_face = cv2.resize(gray_face, (emotion_target_size))
         except:
             continue
-
-        gray_face = preprocess_input(gray_face, True)
+        gray_face = image.img_to_array(gray_face)
         gray_face = np.expand_dims(gray_face, 0)
-        gray_face = np.expand_dims(gray_face, -1)
+        gray_face = preprocess_input(gray_face, True)
+
+        #gray_face = np.expand_dims(gray_face, 0)
+        #gray_face = np.expand_dims(gray_face, -1)
         emotion_prediction = emotion_classifier.predict(gray_face)
-        #print(emotion_prediction)
+        # print(emotion_prediction)
         emotion_probability = np.max(emotion_prediction)
         emotion_label_arg = np.argmax(emotion_prediction)
         emotion_text = emotion_labels[emotion_label_arg]
